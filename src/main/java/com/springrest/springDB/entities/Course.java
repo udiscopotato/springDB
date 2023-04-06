c022dfc8888d09817db0cf8a53ca990677c8b507
Subham Jena <103844138+udiscopotato@users.noreply.github.com>
2023-04-07 00:40:10 +0530

    completed mysql databse connectivity

--- src/main/java/com/springrest/springDB/entities/Course.java
+++ src/main/java/com/springrest/springDB/entities/Course.java
@@ -1,7 +1,12 @@
 package com.springrest.springDB.entities;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+
+@Entity
 public class Course {
 	
+	@Id
 	private long id;
 	private String name;
 	private String description;
--- src/main/java/com/springrest/springDB/service/CourseServiceImplementation.java
+++ src/main/java/com/springrest/springDB/service/CourseServiceImplementation.java
@@ -1,72 +1,79 @@
 package com.springrest.springDB.service;
 
-import java.util.ArrayList;
+//import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.springrest.springDB.dao.CourseDao;
 import com.springrest.springDB.entities.Course;
 
 @Service
 public class CourseServiceImplementation implements CourseService {
-	
-	List<Course> list;
+	@Autowired
+	private CourseDao coursedao;
+//	List<Course> list;
 	
 	public CourseServiceImplementation() {
-		list = new ArrayList<>();
-		list.add(new Course(111, "Core Java", "Basic Programming with Java"));
-		list.add(new Course(222, "Python", "Python with DataScience"));
-		list.add(new Course(333, "GoLang", "Introduction to Go") );
-		list.add(new Course(444, "PHP", "Server side Scripting using PHP"));
+//		list = new ArrayList<>();
+//		list.add(new Course(111, "Core Java", "Basic Programming with Java"));
+//		list.add(new Course(222, "Python", "Python with DataScience"));
+//		list.add(new Course(333, "GoLang", "Introduction to Go") );
+//		list.add(new Course(444, "PHP", "Server side Scripting using PHP"));
 	}
 	@Override
 	public List<Course> getCourses() {
-		return list;
+		return coursedao.findAll();
 	}
 	
 	@Override
 	public Course getCourse(long courseId) {
 		// TODO Auto-generated method stub
-		Course cour=null;
-		for(Course course: list) {
-			if(course.getId() == courseId) {
-				cour=course;
-				break;
-			}
-		}
-		return cour;
+//		Course cour=null;
+//		for(Course course: list) {
+//			if(course.getId() == courseId) {
+//				cour=course;
+//				break;
+//			}
+//		}
+		return coursedao.getReferenceById(courseId);
 	}
 	
 	@Override
 	public Course addCourse(Course course) {
-		list.add(course);
+//		list.add(course);
+		coursedao.save(course);
 		return course;
 	}
 	
 	@Override
 	public Course updateCourse(Course course) {
-		Course c = null;
-		for(Course courses: list) {
-			if(courses.getId() == course.getId()) {
-				c=course;
-				courses.setName(course.getName());
-				courses.setDescription(course.getDescription());
-				break;
-			}
-		}
-		if(c == null) {
-			c=addCourse(course);
-		}
-		return c;
+//		Course c = null;
+//		for(Course courses: list) {
+//			if(courses.getId() == course.getId()) {
+//				c=course;
+//				courses.setName(course.getName());
+//				courses.setDescription(course.getDescription());
+//				break;
+//			}
+//		}
+//		if(c == null) {
+//			c=addCourse(course);
+//		}
+		coursedao.save(course);
+		return course;
 	}
 	
 	@Override
 	public void removeCourse(long courseId) {
-		for(Course course: list) {
-			if(course.getId() == courseId) {
-				list.remove(course);
-			}
-		}
+//		for(Course course: list) {
+//			if(course.getId() == courseId) {
+//				list.remove(course);
+//			}
+//		}
+		Course entity = coursedao.getReferenceById(courseId);
+		coursedao.delete(entity);
 	}
 	
 	
